e7f9558df2a58a1a3e09c404ae4320f6255f5c61
lvlrt <lars@larsveelaert.be>
2022-11-13 22:24:21 +0100

    added requirements

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"path"
 	"regexp"
 	"strings"
+
+	"golang.org/x/exp/slices"
 )
 
 func main() {
@@ -33,19 +35,17 @@ func main() {
 		os.Exit(1)
 	}
 
-	requirementLookupMap := createRequirementLookupMap(requirements)
-
 	err, specificationMap, untaggedSpecifications := createSpecificationMap(testcases)
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 
-	printSpecificationMap(specificationMap, untaggedSpecifications, requirementLookupMap)
+	printSpecificationMap(specificationMap, untaggedSpecifications, requirements)
 
 	if *flags.Store {
 		println(fmt.Sprintf("Storing specification map in %v", *flags.SpecificationsMapOutput))
-		err = storeSpecificationMap(*flags.SpecificationsMapOutput, specificationMap, untaggedSpecifications, requirementLookupMap)
+		err = storeSpecificationMap(*flags.SpecificationsMapOutput, specificationMap, untaggedSpecifications, requirements)
 		if err != nil {
 			fmt.Println(err.Error())
 			os.Exit(1)
@@ -136,23 +136,39 @@ func createSpecificationMap(testcases []TestCase) (error, map[string][]Specifica
 	return nil, specificationMap, untagged
 }
 
-func storeSpecificationMap(filepath string, specificationMap map[string][]Specification, untagged []Specification, requirementLookupMap map[string]Requirement) error {
+func storeSpecificationMap(filepath string, specificationMap map[string][]Specification, untagged []Specification, requirements []Requirement) error {
 	var markdown string
 	markdown += "# Specifications Map (generated)\n\n"
 
-	for tag, specs := range specificationMap {
-		req := tag
-		if val, ok := requirementLookupMap[tag]; ok {
-			req = fmt.Sprintf("%v: %v", tag, val.Description)
+	var tags []string
+
+	//organize based on requirement
+	for _, requirement := range requirements {
+		tags = append(tags, requirement.Tag)
+		markdown += fmt.Sprintf("- **%v: %v**\n", requirement.Tag, requirement.Description)
+		if specs, ok := specificationMap[requirement.Tag]; ok {
+			for _, spec := range specs {
+				markdown += fmt.Sprintf("    - %v *(%v)*\n", spec.Description, spec.File)
+			}
+		} else {
+			markdown += fmt.Sprintf("    (no specifications found)\n")
 		}
-		markdown += fmt.Sprintf("- **%v**\n", req)
+	}
+
+	//organize based on tags with no requirement
+	for tag, specs := range specificationMap {
+		if slices.Contains(tags, tag) {
+			continue
+		}
+		markdown += fmt.Sprintf("- **%v:**(no requirement description available)\n", tag)
 		for _, spec := range specs {
 			markdown += fmt.Sprintf("    - %v *(%v)*\n", spec.Description, spec.File)
 		}
 	}
 
+	//untagged
 	if len(untagged) != 0 {
-		markdown += fmt.Sprintf("- **%v**\n", "(untagged)")
+		markdown += fmt.Sprintf("- **%v**\n", "(untagged specifications)")
 		for _, spec := range untagged {
 			markdown += fmt.Sprintf("    - %v *(%v)*\n", spec.Description, spec.File)
 		}
@@ -176,28 +192,43 @@ func storeSpecificationMap(filepath string, specificationMap map[string][]Specif
 	return nil
 }
 
-func printSpecificationMap(specificationMap map[string][]Specification, untagged []Specification, requirementLookupMap map[string]Requirement) error {
-	for tag, specs := range specificationMap {
-		req := tag
-		if val, ok := requirementLookupMap[tag]; ok {
-			req = fmt.Sprintf("%v: %v", tag, val.Description)
+func printSpecificationMap(specificationMap map[string][]Specification, untagged []Specification, requirements []Requirement) error {
+	var tags []string
+
+	//organize based on requirement
+	for _, requirement := range requirements {
+		tags = append(tags, requirement.Tag)
+		var specs []Specification
+		if val, ok := specificationMap[requirement.Tag]; ok {
+			specs = val
 		}
-		printSpecs(req, specs)
+		printSpecs(fmt.Sprintf("%v: %v", requirement.Tag, requirement.Description), specs)
 	}
 
-	printSpecs("(untagged)", untagged)
+	//organize based on tags with no requirement
+	for tag, specs := range specificationMap {
+		if slices.Contains(tags, tag) {
+			continue
+		}
+		printSpecs(fmt.Sprintf("%v: (no requirement description available)", tag), specs)
+	}
+
+	//untagged
+	if len(untagged) != 0 {
+		printSpecs("(untagged specifications)", untagged)
+	}
 
 	return nil
 }
 
 func printSpecs(title string, specs []Specification) {
-	if len(specs) == 0 {
-		return
-	}
-
 	fmt.Println(fmt.Sprintf("%v", title))
-	for _, spec := range specs {
-		fmt.Println(fmt.Sprintf("  - %v (%v)", spec.Description, spec.File))
+	if specs == nil || len(specs) == 0 {
+		fmt.Println(fmt.Sprintf("  (no specifications found)"))
+	} else {
+		for _, spec := range specs {
+			fmt.Println(fmt.Sprintf("  - %v (%v)", spec.Description, spec.File))
+		}
 	}
 	fmt.Println("")
 }
